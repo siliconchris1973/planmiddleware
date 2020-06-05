4ee54ba64891f61d7678b37c4c39dfc5f9a622c2
Christian Günther <c.guenther@mac.com>
2020-06-05 14:18:53 +0200

    added route skeleton to settings page and began work on settings page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var settingsRouter = require('./routes/settings');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/settings', settingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'COMLINE T&A Middleware for PLAN', subtitle: 'Dashboard' });
 });
 
 module.exports = router;
